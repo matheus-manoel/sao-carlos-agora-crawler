ceb0299c40a5cadf91c215e56a96849c39a62965
Matheus Manoel <matheuscmanoel@gmail.com>
2019-08-23 00:42:04 -0300

    Get links from last news.

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ import requests
 from newsplease import NewsPlease
 from bs4 import BeautifulSoup
 
-from src import get_match_result
+from src import get_match_result, get_links_and_dates
 
 page = requests.get('https://www.saocarlosagora.com.br/ultimas-noticias/')
 soup = BeautifulSoup(page.content, 'html.parser')
-news_list = soup.find_all('div', {'class': 'linkNoticia'})
-news_link 
 
-print(news)
+result = get_links_and_dates(soup)
+
+print(result)
 
 '''
 article = NewsPlease.from_url('https://www.saocarlosagora.com.br/policia/apos-bater-na-esposa-e-em-uma-crianca-de-9-anos-homem-e-agredido-por/117007/')
--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,8 @@ def get_match_result(keywords, article):
         'title_match': any([keyword for keyword in keywords if keyword in article.title]),
         'body_matches': any([keyword for keyword in keywords if keyword in article.text])
     }
+
+def get_links_and_dates(soupfied_last_news):
+    news_list = [titulo.get('href') for titulo in
+                 soupfied_last_news.find_all('a', {'class': 'titulos'})]
+    return news_list
