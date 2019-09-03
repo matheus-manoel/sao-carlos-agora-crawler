6b02651ebba8264e3045427689c12e3d42bf6154
Matheus Manoel <matheuscmanoel@gmail.com>
2019-09-03 14:40:58 -0300

    Finish link and date filtering.

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import requests
+from datetime import date
 
 from newsplease import NewsPlease
 from bs4 import BeautifulSoup
 
-from src import get_match_result, get_links_and_dates
+from src import get_match_result, get_links_and_dates, filter_links_by_date
 
 page = requests.get('https://www.saocarlosagora.com.br/ultimas-noticias/')
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -11,6 +12,9 @@ soup = BeautifulSoup(page.content, 'html.parser')
 result = get_links_and_dates(soup)
 
 print(result)
+print(date.today())
+print(filter_links_by_date(result))
+
 
 '''
 article = NewsPlease.from_url('https://www.saocarlosagora.com.br/policia/apos-bater-na-esposa-e-em-uma-crianca-de-9-anos-homem-e-agredido-por/117007/')
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+
 def get_match_result(keywords, article):
     return {
         'title_match': any([keyword for keyword in keywords if keyword in article.title]),
@@ -5,6 +8,26 @@ def get_match_result(keywords, article):
     }
 
 def get_links_and_dates(soupfied_last_news):
-    news_list = [titulo.get('href') for titulo in
-                 soupfied_last_news.find_all('a', {'class': 'titulos'})]
-    return news_list
+    news_link_list = [titulo.get('href') for titulo in
+                      soupfied_last_news.find_all('a', {'class': 'titulos'})]
+    news_date_list = [lista_data.small.get_text() for lista_data in
+                      soupfied_last_news.find_all('div', {'class': 'listaData'})]
+    return dict(zip(news_link_list, news_date_list))
+
+def _parse_date(raw_date, date=date):
+    text_to_number = {
+        'Jan': 1, 'Fev': 2, 'Mar': 3,
+        'Abr': 4, 'Mai': 5, 'Jun': 6,
+        'Jul': 7, 'Ago': 8, 'Set': 9,
+        'Out': 10, 'Nov': 11, 'Dez': 12,
+    }
+    date_info = raw_date.split()
+    return date(day=int(date_info[0]),
+                month=text_to_number[date_info[1]],
+                year=int(date_info[2]))
+
+def filter_links_by_date(news_and_dates, date=date):
+    return {
+        k: v for k, v in news_and_dates.items()
+        if _parse_date(v) == date.today()
+    }
